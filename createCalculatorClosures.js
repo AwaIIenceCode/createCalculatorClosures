bde248f7e860472589361ad4edd81fbaa516570b
AwallenceCode <awallencecode@gmail.com>
2026-03-18 19:01:13 +0200

    Enhance calculator functionality by adding arithmetic methods and updating main function to demonstrate usage

--- createCalculatorClosures.js
+++ createCalculatorClosures.js
@@ -1,13 +1,43 @@
 // calculator function with using closures
-function createCalculator()
+function createCalculator(userNumber)
 {
+    return {
+        add: function(value)
+        {
+            userNumber += value;
+            return this;
+        },
 
+        subtract: function(value)
+        {
+            userNumber -= value;
+            return this;
+        },
+
+        multiply: function(value)
+        {
+            userNumber *= value;
+            return this;
+        },
+
+        divide: function(value)
+        {
+            userNumber /= value;
+            return this;
+        },
+
+        getResult: function(value)
+        {
+            return userNumber;
+        }
+    };
 } 
 
 // main function for start the program
 function main()
 {
-
+    let calc = createCalculator(10);
+    console.log(calc.add(5).multiply(2).subtract(3).getResult());
 }
 
 main();
